b45335894a21c4a66ad58649151b9eea15e704f0
JEFF\trann <trannamhaibp>
2017-07-10 17:12:31 +0700

    commit paging for list

---
+++ TheTechShop.Web/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheTechShop.Web.Infrastructure.Core
+{
+    public class PaginationSet<T>
+    {
+        public int Page { set; get; }
+
+        public int Count
+        {
+            get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+
+        public int TotalPages { set; get; }
+        public int TotalCount { set; get; }
+        public IEnumerable<T> Items { set; get; }
+    }
+}
