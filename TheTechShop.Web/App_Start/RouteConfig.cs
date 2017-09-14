e91f275cfc6dc0dc3be250743afca47e6a6a47d2
JEFF\trann <trannamhaibp>
2017-09-14 23:19:58 +0700

    Final

--- TheTechShop.Web/App_Start/RouteConfig.cs
+++ TheTechShop.Web/App_Start/RouteConfig.cs
@@ -7,38 +7,37 @@ namespace TheTechShop.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
+         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+         routes.MapRoute(
          name: "Login",
          url: "dang-nhap.html",
          defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
          namespaces: new string[] { "TheTechShop.Web.Controllers" }
-     );
-            routes.MapRoute(
-           name: "About",
-           url: "gioi-thieu.html",
-           defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
-           namespaces: new string[] { "TheTechShop.Web.Controllers" }
-       );
-            routes.MapRoute(
-             name: "Product Category",
-             url: "{alias}.pc-{id}.html",
-             defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
-               namespaces: new string[] { "TheTechShop.Web.Controllers" }
+        );
+         routes.MapRoute(
+         name: "About",
+         url: "gioi-thieu.html",
+         defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+         namespaces: new string[] { "TheTechShop.Web.Controllers" }
+        );
+         routes.MapRoute(
+         name: "Product Category",
+         url: "{alias}.pc-{id}.html",
+         defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+         namespaces: new string[] { "TheTechShop.Web.Controllers" }
          );
-
-            routes.MapRoute(
-             name: "Product",
-             url: "{alias}.p-{productId}.html",
-             defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
-               namespaces: new string[] { "TheTechShop.Web.Controllers" }
+         routes.MapRoute(
+         name: "Product",
+         url: "{alias}.p-{productId}.html",
+         defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
+         namespaces: new string[] { "TheTechShop.Web.Controllers" }
          );
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                  namespaces: new string[] { "TheTechShop.Web.Controllers" }
-            );
+         routes.MapRoute(
+         name: "Default",
+         url: "{controller}/{action}/{id}",
+         defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+         namespaces: new string[] { "TheTechShop.Web.Controllers" }
+        );
         }
     }
 }
